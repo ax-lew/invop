f8e08d40af9f028b60b2d0cb583a6e0fc4a89ddf
alew <axel.lew@hotmail.com>
2018-07-10 20:35:12 -0300

    using cplex

--- Ej23/MilkCollection.cpp
+++ Ej23/MilkCollection.cpp
@@ -10,6 +10,13 @@
 using namespace std;
 
 
+struct Farm{
+    int east;
+    int north;
+    bool everyDay;
+    int toCollect;
+};
+
 struct Problema{
 	vector< vector<float> > distances;
 	map<int,Farm> farmsInfo;
@@ -17,13 +24,6 @@ struct Problema{
 	Problema(){}
 };
 
-
-struct Farm{
-    int east;
-    int north;
-    bool everyDay;
-    int toCollect;
-};
 
 bool getBooleanValue(string s) {
     if (s == "true") {
@@ -69,10 +69,10 @@ map<int,Farm> getFarmInfo() {
     return m;
 }
 
-vector< vector<float> > createDistancesMatrix(map<int,Farm> farmData){
-    vector< vector<float> > distances(farmData.size(), vector<float> (farmData.size(), 0));
-    for (map<int,Farm>::iterator it=farmData.begin(); it!=farmData.end(); ++it){
-        for (map<int,Farm>::iterator it2=farmData.begin(); it!=farmData.end(); ++it){
+vector< vector<float> > createDistancesMatrix(map<int,Farm> farmsInfo){
+    vector< vector<float> > distances(farmsInfo.size(), vector<float> (farmsInfo.size(), 0));
+    for (map<int,Farm>::iterator it=farmsInfo.begin(); it!=farmsInfo.end(); ++it){
+        for (map<int,Farm>::iterator it2=farmsInfo.begin(); it!=farmsInfo.end(); ++it){
             distances[it->first][it2->first] = getDistances(it->second, it2->second);
         }
     }
@@ -180,16 +180,20 @@ void solveMIP(Problema * prob){
 
     // Definimos la variables del problema con su valor en la funcion objetivo, lower bound, upper bound y coltype que indica el tipo de variable
     // CPLEX maneja un arreglo de variables. Para esto consideramos las variables en el orden X_11, X_12, X_13, .... , X_21, X_22,..., XN1, .... , Y_1 , Y_2...
-    for(int i=0;i<cantvar;i++){
-        // Las variables Y tiene un 1 en la funcion objetivo y las X un 0
-        obj[i] = (i>=prob->N*prob->bestObj)?1.:0.;
-        
-        // Todas las variables son binarias con cotas 0 y 1.
-        lb[i] = 0.;
-        ub[i] = 1.;
-        coltype[i] = 'B';
+    for (int days=0; days<2; days++){
+    	for(int i=0;i<prob->farmsQty;i++){
+	    	for(int j=i+1;j<prob->farmsQty;j++){
+	    		int offset = days*prob->farmsQty*(prob->farmsQty-1)/2;
+	    		int index = i*prob->farmsQty+j+offset;
+	    		obj[index] = prob->distances[i][j];
+	    		lb[index] = 0.;
+		        ub[index] = 1.;
+		        coltype[index] = 'B';
+	    	}
+	    }
     }
-    
+
+ 
     
     // Con esta llamada se crean todas las columnas de nuestro problema
     status = CPXnewcols(env, lp, cantvar, obj, lb, ub, coltype, NULL);
@@ -198,99 +202,26 @@ void solveMIP(Problema * prob){
     
     ////////////////////////////
     // Una vez armadas todas las variables  y la funcion objetivo, faltan las restricciones
-    
-    
-    //Primera restriccion: Dos items que son vecinos no pueden tener un mismo color k
-    // X_ik + X_jk <= Y_k
-    for(int i=0;i<prob->N;i++){//Recorro los vertices
-        for(int j=0;j<(int)prob->ejes[i].size();j++){ // Por cada vertice miro solo sus vecinos
-            if(i>=prob->ejes[i][j])continue; // Esta linea esta para definir una sola vez la restriccion para cada par de vertices
-            for(int k=0;k<prob->bestObj;k++){ // Recorro todos los colores
-                
-                // Como CPLEX deja meter muchas restricciones en una misma llamada, hace falta un arreglo rmatbeg que indique cada restriccion que ponemos donde comienza
-                // En general siempre pondremos una sola restriccion en cada llamada por lo que este arreglo tendra una sola posicion con valor 0
-                rmatbeg[0] = 0;
-                
-                rhs[0] = 0.0;// El valor del lado derecho de la restriccion
-                sense[0] = 'L'; // El sentido de la restriccion (menor igual, igual, mayor igual)
-                
-                
-                // Ahora solo falta indicar que variables estan en el lado izquierdo y con que valor.
-                // rmatval tendra el valor de la variable
-                // rmatind tendra el indice de la variable
-                
-                rmatval[0] = 1.0;
-                rmatind[0] = i*prob->bestObj+k;
-                rmatval[1] = 1.0;
-                rmatind[1] = prob->ejes[i][j]*prob->bestObj+k;
-                rmatval[2] = -1.0;
-                rmatind[2] = prob->N*prob->bestObj+k;
-                
-                // Una vez armada toda la restriccion se hace la siguiente llamada para agregarla al modelo
-                status = CPXaddrows(env, lp, 0, 1, 3, rhs, sense, rmatbeg, rmatind, rmatval, NULL, NULL);
-                if(status)exit(-1);
-            }
-        }
-    }
-    
-    
-    //Restriccion de un color por nodo
-    //Sum_k X_ik = 1 para todo i
-    for(int i=0;i<prob->N;i++){
-        rmatbeg[0] = 0;
+    for (int days=0; days<2; days++){
+	    rmatbeg[0] = 0;
         rhs[0] = 1.0;
         sense[0] = 'E';
         int count = 0;
-        for(int j=0;j<prob->bestObj;j++){
-            rmatval[count] = 1.0;
-            rmatind[count++] = i*prob->bestObj+j;
-        }
-        status = CPXaddrows(env, lp, 0, 1, count, rhs, sense, rmatbeg, rmatind, rmatval, NULL, NULL);
-        if(status)exit(-1);
-    }
-    
-    // Restriccion que rompe simetria
-    // No hace falta para que el modelo sea correcto, pero se la podemos agregar como restriccion para obtener poliedros mas chicos
-    // Y_k >= Y_k+1
-    for(int i=0;i<prob->bestObj-1;i++){
-        rmatbeg[0] = 0;
-        rhs[0] = 0.;
-        sense[0] = 'G';
-        rmatval[0] = 1.0;
-        rmatind[0] = prob->N*prob->bestObj+i;
-        rmatval[1] = -1.0;
-        rmatind[1] = prob->N*prob->bestObj+i+1;
-        status = CPXaddrows(env, lp, 0, 1, 2, rhs, sense, rmatbeg, rmatind, rmatval, NULL, NULL);
+	    for(int i=0;i<prob->farmsQty;i++){
+	    	for(int j=i+1;j<prob->farmsQty;j++){
+	    		int offset = days*prob->farmsQty*(prob->farmsQty-1)/2;
+	    		int index = i*prob->farmsQty+j+offset;
+	    		rmatval[index] = 1.0;
+	    		rmatind[index] = prob->farmsInfo[i];
+	    	}
+	    }
+	    status = CPXaddrows(env, lp, 0, 1, count, rhs, sense, rmatbeg, rmatind, rmatval, NULL, NULL);
         if(status)exit(-1);
     }
     
 
-    /////////////////////////////
-    // Este sector de codigo permite proveerle a CPLEX una solucion factible de entrada
-    status = CPXsetintparam (env, CPX_PARAM_ADVIND, 1);
-    if(status)exit(-1);
-    
-    
-    int beg[1] = {0};
-    int nzcnt = prob->N+prob->bestObj;
-    int* varindices = (int*)malloc(sizeof(int)*nzcnt);
-    for(int i=0;i<prob->N;i++){
-        varindices[i] = i*prob->bestObj+prob->colores[i];
-    }
-    for(int i=0;i<prob->bestObj;i++){
-        varindices[i+prob->N] = i+prob->N*prob->bestObj;
-    }
-    
-    
-    double* values = (double*)malloc(sizeof(double)*nzcnt);
-    for(int i=0;i<nzcnt;++i)values[i]=1.0;
-    int effortlevel[1] = {CPX_MIPSTART_AUTO};
-    status = CPXaddmipstarts(env, lp, 1, nzcnt, beg, varindices, values, effortlevel, NULL);
 
-    
-    free(varindices);
-    free(values);
-    /////////////////////////////
+   
     
     
     /////////////////////////////
@@ -317,15 +248,15 @@ void solveMIP(Problema * prob){
 
     
     // Armamos la asignacion de colores en nuestra estructura con la informacion que nos trajimos de CPLEX
-    if(prob->bestObj>=objval){
-        for(int i=0; i<prob->N; ++i){
-            for(int j=0; j<prob->bestObj; ++j){
-                if(x[i*prob->bestObj+j]>0.001){
-                    prob->colores[i]=j;
-                }
-            }
-        }
-        prob->bestObj = objval;
+    cout << "objval" << objval << endl;
+
+    for (int days=0; days<2; days++){
+    	for(int i=0;i<prob->farmsQty;i++){
+	    	for(int j=i+1;j<prob->farmsQty;j++){
+	    		int offset = days*prob->farmsQty*(prob->farmsQty-1)/2;
+	    		int index = i*prob->farmsQty+j+offset;
+	    		cout << "index" << x[index] << endl;
+	    }
     }
     
     
